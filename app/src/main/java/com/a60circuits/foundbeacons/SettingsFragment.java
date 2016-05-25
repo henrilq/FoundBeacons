ea2e2248884a4c0b85059a27fc9a84525e8836f1
henrilq <henrilq@gmx.fr>
2016-05-25 11:05:23 +0200

    settings screen

--- app/src/main/java/com/a60circuits/foundbeacons/SettingsFragment.java
+++ app/src/main/java/com/a60circuits/foundbeacons/SettingsFragment.java
@@ -3,17 +3,44 @@ package com.a60circuits.foundbeacons;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 /**
  * Created by zoz on 17/05/2016.
  */
 public class SettingsFragment extends Fragment{
+
+    private Switch gpsSwitch;
+    private Switch notificationSwitch;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return  inflater.inflate(R.layout.settings_fragment,container,false);
+        View view = inflater.inflate(R.layout.settings_fragment,container,false);
+        gpsSwitch = (Switch) view.findViewById(R.id.gps_switch);
+        notificationSwitch = (Switch) view.findViewById(R.id.notification_switch);
+
+        gpsSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gpsSwitch.setChecked(true);
+            }
+        });
+
+        notificationSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(notificationSwitch.isChecked()){
+                    
+                }
+            }
+        });
+
+        return view;
     }
 }
